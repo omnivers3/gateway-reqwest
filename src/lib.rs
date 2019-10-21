79ddc9c387507b42a336bdda643821f850a7a762
Perry Birch <perrybirch@vizidrix.com>
2019-10-21 12:28:59 -0700

    refactoring to req/resp

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ extern crate log;
 extern crate reqwest;
 extern crate serde;
 extern crate serde_json;
+extern crate url;
 
 #[cfg(test)]
 #[macro_use]
@@ -14,13 +15,12 @@ extern crate env_logger;
 
 extern crate gateway;
 
+// use std::convert::{ TryInto };
 use std::fmt;
-use std::marker::PhantomData;
 
-use gateway::{ parse_url, Message, Request, Service, ServiceError };
+use gateway::{ parse_url, Message, Service, ServiceError };
 use gateway::contracts::{ v1 };
 
-pub type GatewayError = gateway::Error<serde_json::error::Error, serde_json::error::Error>;
 
 #[derive(Debug, Default)]
 /// Captures reqwest metadata for diagnosing issues with calls
@@ -29,20 +29,23 @@ pub struct ErrorContext {
     error: Option<reqwest::Error>,
 }
 
+pub type GatewayError = gateway::Error<serde_json::error::Error, serde_json::error::Error>;
+
+pub type ServiceResult<TResponse> = Result<TResponse, ServiceError<ErrorContext, GatewayError>>;
+
 /// Service implementation using Reqwest for proxying to the backing api(s)
-pub struct ReqwestService<TResult> {
-    _result: PhantomData<TResult>,
+pub struct ReqwestService {
     url: url::Url,
 }
 
-impl<TResult> fmt::Debug for ReqwestService<TResult> {
+impl fmt::Debug for ReqwestService {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "ReqwestService {{ url: {} }}", self.url)
     }
 }
 
-impl<TResult> ReqwestService<TResult> {
-    pub fn with_url(url_str: &str) -> Result<Self, ServiceError<ErrorContext, GatewayError>> {
+impl ReqwestService {
+    pub fn with_url(url_str: &str) -> ServiceResult<Self> {
         parse_url(url_str)
             .map_err(|error| {
                 ServiceError {
@@ -52,74 +55,119 @@ impl<TResult> ReqwestService<TResult> {
             })
             .map(|url| {
                 ReqwestService {
-                    _result: PhantomData,
                     url,
                 }
             })
     }
 }
 
-impl<TResult> Service<TResult> for ReqwestService<TResult> {
+#[derive(Debug)]
+pub enum Request {
+    Get {
+        path: String,
+    },
+}
+
+impl From<()> for Request {
+    fn from(_: ()) -> Request {
+        Request::Get {
+            path: "".to_owned()
+        }
+    }
+}
+
+fn exec_request<TRequest, TResponse>(svc: &ReqwestService, req: TRequest) -> ServiceResult<reqwest::Response> where
+    TRequest: Into<Request>,
+{
+    let url = svc.url.to_owned();
+    let req = req.into();
+
+    match req {
+        Request::Get { path } => {
+            let url = url
+                .join(&path)
+                .map_err(|error| ServiceError {
+                    context: ErrorContext {
+                        response: None,
+                        error: None,
+                    },
+                    error: GatewayError::UrlParseFailed(error),
+                })?;
+            println!("Reqwest Get: {:?}", url);
+            reqwest::get(url.as_str())
+                .map_err(|error| ServiceError {
+                    context: ErrorContext {
+                        response: None,
+                        error: Some(error),
+                    },
+                    error: GatewayError::RequestFailed,
+                })
+        }
+    }
+}
+
+fn extract_text(mut response: reqwest::Response) -> ServiceResult<(reqwest::Response, String)> {
+    match response.text() {
+        Ok(text) => Ok((response, text)),
+        Err(error) => Err(ServiceError {
+            context: ErrorContext {
+                response: Some(response),
+                error: Some(error),
+            },
+            error: GatewayError::ReadBodyFailed
+        }),
+    }
+}
+
+fn validate_status((response, text): (reqwest::Response, String)) -> ServiceResult<(reqwest::Response, String)> {
+    if response.status().eq(&200) {
+        Ok((response, text))
+    } else {
+        Err (ServiceError {
+            context: ErrorContext {
+                response: Some(response),
+                error: None,
+            },
+            error: GatewayError::ResultFailed {
+                message: serde_json::from_str::<v1::Message>(&text).map(Message::V1),
+            }
+        })
+    }
+}
+
+fn parse_response<TResponse>((response, text): (reqwest::Response, String)) -> ServiceResult<TResponse> where
+    TResponse: serde::de::DeserializeOwned + std::fmt::Debug
+{
+    serde_json::from_str::<TResponse>(&text)
+        .map_err(|error| ServiceError {
+            context: ErrorContext {
+                response: Some(response),
+                error: None,
+            },
+            error: GatewayError::InvalidPayload {
+                error,
+                message: serde_json::from_str::<v1::Message>(&text).map(Message::V1),
+            }
+        })
+}
+
+impl Service for ReqwestService {
+    type TRequest = Request;
     type TContext = ErrorContext;
     type TPayloadSerdeError = serde_json::error::Error;
     type TMessageSerdeError = serde_json::error::Error;
 
-    fn get(&self, req: Request) -> Result<TResult, ServiceError<ErrorContext, GatewayError>> where
-        TResult: serde::de::DeserializeOwned+ std::fmt::Debug,
+    fn exec<TRequest, TResponse>(&self, req: TRequest) -> ServiceResult<TResponse> where
+        TRequest: Into<Self::TRequest> + std::fmt::Debug,
+        TResponse: serde::de::DeserializeOwned + std::fmt::Debug,
     {
+        println!("REQWEST\tAPI REQ: [{:?}]", req);
         debug!("REQWEST\tAPI REQ: [{:?}]", req);
 
-        let uri: &str = match req {
-            Request::Unit => {
-                self.url.as_str()
-            }
-        };
-        
-        // Call the configured url
-        let mut response = reqwest::get(uri)
-            .map_err(|error| ServiceError {
-                context: ErrorContext {
-                    response: None,
-                    error: Some(error),
-                },
-                error: GatewayError::RequestFailed,
-            })?;
-        // Try to extract the response text to parse
-        let text_result = response.text();
-        if let Err (error) = text_result {
-            return Err (ServiceError {
-                context: ErrorContext {
-                    response: Some(response),
-                    error: Some(error),
-                },
-                error: GatewayError::ReadBodyFailed
-            })
-        }
-        let text = text_result.unwrap();
-        // Ensure only 200 OK responses return success
-        if !response.status().eq(&200) {
-            return Err (ServiceError {
-                context: ErrorContext {
-                    response: Some(response),
-                    error: None,
-                },
-                error: GatewayError::ResultFailed {
-                    message: serde_json::from_str::<v1::Message>(&text).map(Message::V1),
-                }
-            })
-        }
-        // Try to parse the payload from the body text
-        serde_json::from_str::<TResult>(&text)
-            .map_err(|error| ServiceError {
-                context: ErrorContext {
-                    response: Some(response),
-                    error: None,
-                },
-                error: GatewayError::InvalidPayload {
-                    error,
-                    message: serde_json::from_str::<v1::Message>(&text).map(Message::V1),
-                }
-            })
+        exec_request::<TRequest, TResponse>(self, req)
+            .and_then(extract_text)
+            .and_then(validate_status)
+            .and_then(parse_response)
     }
 }
 
@@ -128,7 +176,7 @@ mod tests {
     // use mockito::{ mock, Matcher };
     use mockito::{ mock };
 
-    use super::{ GatewayError, Request, ReqwestService, Service };
+    use super::{ GatewayError, ReqwestService, Service };
 
     fn init() {
         let _ = env_logger::builder().is_test(true).try_init();
@@ -137,7 +185,7 @@ mod tests {
     #[test]
     fn fail_ctor_with_empty_url() {
         init();
-        match ReqwestService::<()>::with_url("") {
+        match ReqwestService::with_url("") {
             Ok (result) => assert!(false, "should have failed empty url but was [{:?}]", result),
             Err (service_error) => {
                 match service_error.error {
@@ -160,8 +208,8 @@ mod tests {
             .expect(1)
             .create();
 
-        match ReqwestService::<()>::with_url("http://www.foo.net/return_error_for_404")
-            .and_then(|svc| svc.get(Request::Unit))
+        match ReqwestService::with_url("http://www.foo.net/return_error_for_404")
+            .and_then(|svc| svc.exec::<(), ()>(()))
         {
             Ok (result) => assert!(false, "should have detected invalid status but was [{:?}]", result),
             Err (service_error) => {
@@ -182,8 +230,8 @@ mod tests {
             .expect(1)
             .create();
 
-        match ReqwestService::<()>::with_url("http://www.foo.net/return_error_for_500")
-            .and_then(|svc| svc.get(Request::Unit))
+        match ReqwestService::with_url("http://www.foo.net/return_error_for_500")
+            .and_then(|svc| svc.exec::<(), ()>(()))
         {
             Ok (result) => assert!(false, "should have detected invalid status but was [{:?}]", result),
             Err (service_error) => {
@@ -205,8 +253,8 @@ mod tests {
             .expect(1)
             .create();
 
-        match ReqwestService::<()>::with_url("http://www.foo.net/return_error_for_invalid_payload")
-            .and_then(|svc| svc.get(Request::Unit))
+        match ReqwestService::with_url("http://www.foo.net/return_error_for_invalid_payload")
+            .and_then(|svc| svc.exec::<(), ()>(()))
         {
             Ok (result) => assert!(false, "should have detected invalid status but was [{:?}]", result),
             Err (service_error) => {
@@ -233,8 +281,8 @@ mod tests {
             .expect(1)
             .create();
 
-        match ReqwestService::<Temp>::with_url("http://www.foo.net/parse_payload_successfully")
-            .and_then(|svc| svc.get(Request::Unit))
+        match ReqwestService::with_url("http://www.foo.net/parse_payload_successfully")
+            .and_then(|svc| svc.exec::<(), Temp>(()))
         {
             Ok (result) => assert_eq!(10, result.foo),
             Err (service_error) => {
